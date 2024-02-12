7f139a9833a8cbf345f7ae86b8b990ab1cf109e3
afontana405 <alex13579@icloud.com>
2024-02-12 17:31:10 -0600

    fixed semicolon

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -75,7 +75,8 @@ const renderActiveNote = () => {
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
-    text: noteText.value
+    text: noteText.value,
+    id: activeNote.id,
   };
   saveNote(newNote).then(() => {
     getAndRenderNotes();
