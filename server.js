110a43672dd965fce909dc3413ff37260a69c081
afontana405 <alex13579@icloud.com>
2024-02-19 15:32:20 -0600

    moved routes to folders

--- server.js
+++ server.js
@@ -1,45 +1,17 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
-const path = require('path')
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const notes = require('./db/db.json')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-})
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-app.post('/api/notes', (req, res) => {
-  req.body.id = uuidv4()
-  notes.push(req.body);
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-  res.json(notes)
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-  for (let i = 0; i < notes.length; i++) {
-    if (notes[i].id === req.params.id) {
-      notes.splice(i, 1);
-    }
-  }
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-  res.json(notes)
-})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
